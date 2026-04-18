c6bcb5c55068f3df92036e02286d78c1219dd1eb
Eduard Gumen <hydrospirt@mail.ru>
2026-04-18 23:18:07 +0300

    Add license and copyright information to main.py

--- cat_bot/main.py
+++ cat_bot/main.py
@@ -1,3 +1,21 @@
+# Cat Time Bot Z — асинхронный Telegram-бот для учета рабочего времени
+# и геймификации активности IT-специалистов.
+# Copyright (C) 2026 https://github.com/1517mb
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version. [cite: 83, 84]
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details. [cite: 85, 86, 87]
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. [cite: 88, 89]
+
 import argparse
 import asyncio
 import logging
